ec47da2e2b42036b4d639a193a1b7b867a7bcd10
LordSA <96365910+LordSA@users.noreply.github.com>
2022-01-28 21:40:09 +0530

    Create gtranslator.py

---
+++ plugins/Tools/gtranslator.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# Copyright (C) @ZauteKm
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from googletrans import Translator
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from .list import list
+from database.gtrans_mdb import find_one
+
+@Client.on_message(filters.command(["tr"]))
+async def left(client,message):
+	if (message.reply_to_message):
+		try:
+			lgcd = message.text.split("/tr")
+			lg_cd = lgcd[1].lower().replace(" ", "")
+			tr_text = message.reply_to_message.text
+			translator = Translator()
+			translation = translator.translate(tr_text,dest = lg_cd)
+			hehek = InlineKeyboardMarkup(
+                                [
+                                    [
+                                        InlineKeyboardButton(
+                                            text=f"translated from {translation.src} to {translation.dest}", url="https://cloud.google.com/translate/docs/languages"
+                                        )
+                                    ],
+                                ]
+                            )
+			try:
+				for i in list:
+					if list[i]==translation.src:
+						fromt = i
+					if list[i] == translation.dest:
+						to = i 
+				await message.reply_text(f"```{translation.text}```", reply_markup=hehek, reply_to_message_id=message.reply_to_message.message_id)
+			except:
+			   	await message.reply_text(f"```{translation.text}```", reply_markup=hehek, reply_to_message_id=message.reply_to_message.message_id)
+			
+
+		except :
+			print("error")
+	else:
+			 ms = await message.reply_text("You can Use This Command by using reply to message")
+			 await ms.delete()
