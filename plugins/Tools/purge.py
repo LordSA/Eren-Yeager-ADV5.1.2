91017d2453a9deff1fc959c79288d93f6b742219
LordSA <96365910+LordSA@users.noreply.github.com>
2022-01-29 08:22:59 +0530

    Update purge.py

--- plugins/Tools/purge.py
+++ plugins/Tools/purge.py
@@ -4,8 +4,8 @@ Syntax: .purge"""
 import asyncio
 from pyrogram import Client, filters
 
-from plugins.josprojects.help_func.admin_check import admin_check
-from plugins.josprojects.help_func.cust_p_filters import f_onw_fliter
+from plugins.Tools.help_func.admin_check import admin_check
+from plugins.Tools.help_func.cust_p_filters import f_onw_fliter
 
 TG_MAX_SELECT_LEN = 100
 
