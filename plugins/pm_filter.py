2ea1d9641ed36fa5546ac9c20abe1dab7072db6f
LordSA <96365910+LordSA@users.noreply.github.com>
2022-01-31 16:40:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -603,7 +603,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.messege.edit_text(
-            text=script.JSON_TXT
+            text=script.JSON_TXT,
             disable_web_page_preview=True,
             reply_markup=reply_markup,
             parse_mode='html'
