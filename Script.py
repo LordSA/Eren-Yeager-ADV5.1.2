593085ab72d4e20e32afdc6414e03bb02211e27c
LordSA <96365910+LordSA@users.noreply.github.com>
2022-01-31 16:19:04 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -109,7 +109,8 @@ Need to delete lots of messages? That's what purges are for!
 • IMDb should have admin privillage.
 • These commands works on group.
 • These commands can be used by Only admin."""
-    JSON_TXT= """Help: <b>JSON</b>
+    JSON_TXT = """Help: <b>JSON</b>
+
 <b>NOTE:</b>
     ADMIN_TXT = """Help: <b>Admin mods</b>
 
