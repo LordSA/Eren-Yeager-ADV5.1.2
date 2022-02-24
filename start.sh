ce7162cc15bb5ee80969329711909659c6ccbcbf
LordSA <96365910+LordSA@users.noreply.github.com>
2022-02-24 17:38:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LordSA/movie-world.git /MovieWorld
+  git clone https://github.com/LordSA/movie-world.git /movie-world
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /MovieWorld
+  git clone $UPSTREAM_REPO /movie-world
 fi
-cd /MovieWorld
+cd /movie-world
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
