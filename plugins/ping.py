6f10e2bb47ccd0939010aa749f29a26bf2b95db2
LordSA <96365910+LordSA@users.noreply.github.com>
2022-02-28 21:16:08 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -4,7 +4,7 @@ import asyncio
 from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import Message
-from config import bot, call_py, HNDLR, contact_filter
+from info import bot, call_py, HNDLR, contact_filter
 from time import time
 from datetime import datetime
 
