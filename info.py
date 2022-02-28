f545f343f0ff497556cb2cdc9dda830359a0a9b0
LordSA <96365910+LordSA@users.noreply.github.com>
2022-02-28 19:12:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,6 +32,8 @@ auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
+HNDLR = os.getenv("HNDLR", "!")
+GROUP_MODE = os.getenv("GROUP_MODE", "True")
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
