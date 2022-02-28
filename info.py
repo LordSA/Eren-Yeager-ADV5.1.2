57d72feb492eb406544250dc23d85add7706a460
LordSA <96365910+LordSA@users.noreply.github.com>
2022-02-28 19:31:16 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,6 +69,11 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
+if GROUP_MODE == ("True" or "true"):
+    grp = True
+else:
+    grp = False
+
 GRPPLAY = grp
 bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="plugins"))
 call_py = PyTgCalls(bot)
