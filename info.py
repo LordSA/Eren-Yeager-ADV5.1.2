c2de770ec71e49ea2df4379a8397e0019970e6e6
LordSA <96365910+LordSA@users.noreply.github.com>
2022-02-28 19:09:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,5 +1,8 @@
 import re
 from os import environ
+from dotenv import load_dotenv
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
 
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
