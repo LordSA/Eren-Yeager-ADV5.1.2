8887b3e826ff30ecffeeeac385ce8f2742431850
LordSA <96365910+LordSA@users.noreply.github.com>
2022-02-28 19:34:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,6 +69,11 @@ LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies 
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
+contact_filter = filters.create(
+    lambda _, __, message:
+    (message.from_user and message.from_user.is_contact) or message.outgoing
+)
+
 if GROUP_MODE == ("True" or "true"):
     grp = True
 else:
