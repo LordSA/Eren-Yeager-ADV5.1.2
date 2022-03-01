52bdfbc1807a4dc95837d1196946bdfe490a20a4
LordSA <96365910+LordSA@users.noreply.github.com>
2022-03-01 11:36:20 +0530

    Update song_video.py

--- plugins/Tools/song_video.py
+++ plugins/Tools/song_video.py
@@ -1,5 +1,20 @@
-# Made by SA
+#!/usr/bin/env python3
+# Copyright (C) @Lord SA
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from __future__ import unicode_literals
+
 import asyncio
 import math
 import os
@@ -84,7 +99,7 @@ def a(client, message: Message):
         m.delete()
         message.delete()
     except Exception as e:
-        m.edit('**An Error Occured. Please Report This To @mwpro11 !!**')
+        m.edit('**An Error Occured. Please Report This To @JOSPSupport !!**')
         print(e)
     try:
         os.remove(audio_file)
