1dc3de1e9f76923ab5f7dca5a0b08373d54dd11d
LordSA <96365910+LordSA@users.noreply.github.com>
2022-03-02 23:01:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -2,6 +2,7 @@ import os
 import logging
 import random
 import asyncio
+import aiohttp
 from Script import script
 from pyrogram import Client, filters
 from pyrogram.errors import ChatAdminRequired, FloodWait
@@ -523,3 +524,114 @@ async def save_template(client, message):
     template = message.text.split(" ", 1)[1]
     await save_group_settings(grp_id, 'template', template)
     await sts.edit(f"Successfully changed template for {title} to\n\n{template}")
+    
+    
+m = None
+i = 0
+a = None
+query = None
+
+
+@Client.on_message(filters.command(["torrent", "tor"]))
+async def torrent(_, message):
+    global m
+    global i
+    global a
+    global query
+    try:
+        await message.delete()
+    except:
+        pass
+    if len(message.command) < 2:
+        await message.reply_text("`/torrent <Movie Name>`")
+        return
+    query = message.text.split(None, 1)[1].replace(" ", "%20")
+    m = await message.reply_text("Searching\nThis might take a while")
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"https://itor.api-zero.workers.dev/?name={query}") \
+                    as resp:
+                a = json.loads(await resp.text())
+    except:
+        await m.edit("Found Nothing.")
+        return
+    result = (
+        f"**Page - {i+1}**\n\n"
+        f"**🎬 Name :** {a[i]['name']}\n"
+        f"**🧲 Link :** `{a[i]['link']}`\n\n\n"
+    )
+    await m.edit(
+        result,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(f"{emoji.CROSS_MARK} Close",
+                                         callback_data="close_data"),
+                    InlineKeyboardButton(f"Next ⏩",
+                                         callback_data="next_tor")
+                ]
+            ]
+        ),
+        parse_mode="markdown",
+    )
+
+
+@Client.on_callback_query(filters.regex("next_tor"))
+async def callback_query_next(_, message):
+    global i
+    global m
+    global a
+    global query
+    i += 1
+    result = (
+        f"**Page - {i+1}**\n\n"
+        f"**🎬 Name :** {a[i]['name']}\n"
+        f"**🧲 Link :** `{a[i]['link']}`\n\n\n"
+    )
+    await m.edit(
+        result,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(f"⏪ Back",
+                                         callback_data="back_tor"),
+                    InlineKeyboardButton(f"{emoji.CROSS_MARK}",
+                                         callback_data="close_data"),
+                    InlineKeyboardButton(f"Next ⏩",
+                                         callback_data="next_tor")
+                    
+                ]
+            ]
+        ),
+        parse_mode="markdown",
+    )
+
+
+@Client.on_callback_query(filters.regex("back_tor"))
+async def callback_query_previous(_, message):
+    global i
+    global m
+    global a
+    global query
+    i -= 1
+    result = (
+        f"**Page - {i+1}**\n\n"
+        f"**🎬 Name:** {a[i]['name']}\n"
+        f"**🧲 Link:** `{a[i]['link']}`\n\n\n"
+    )
+    await m.edit(
+        result,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(f"⏪ Back",
+                                         callback_data="back_tor"),
+                    InlineKeyboardButton(f"{emoji.CROSS_MARK}",
+                                         callback_data="close_data"),
+                    InlineKeyboardButton(f"Next ⏩",
+                                         callback_data="next_tor")
+                ]
+            ]
+        ),
+        parse_mode="markdown",
+    )
