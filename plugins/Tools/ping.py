561721b0040acdafbea64b4f6cc46376266931c0
LordSA <96365910+LordSA@users.noreply.github.com>
2022-03-03 15:56:03 +0530

    Update ping.py

--- plugins/Tools/ping.py
+++ plugins/Tools/ping.py
@@ -4,27 +4,27 @@ Syntax: .ping"""
 import time
 import random
 from pyrogram import Client, filters
-from info import COMMAND_HAND_LER
+from info import HNDLR
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 # -- Constants -- #
-ALIVE = "എന്നെ ചീത്ത വിളിക്കു... വേണമെങ്കിൽ നല്ല ഇടി ഇടിക്കു... പക്ഷെ ഉപദേശിക്കരുത്....." 
+ALIVE = "എന്തിനാടാ start ഉണ്ടാകുമ്പോൾ എന്നെ നേക്കി എൻ്റെ വായിൽ ഉള്ളത് കേൾക്കുന്നത്... എന്നാ പരിപാടിയാ..." 
 HELP = "ദൈവമേ എന്നെ മാത്രം രക്ഷിക്കണേ...."
 REPO = "നമ്മൾ നമ്മൾ പോലുമറിയാതെ അധോലോകം ആയി മാറിക്കഴിഞ്ഞിരിക്കുന്നു ഷാജിയേട്ടാ..."
 # -- Constants End -- #
 
 
-@Client.on_message(filters.command("alive", COMMAND_HAND_LER) & f_onw_fliter)
+@Client.on_message(filters.command("alive", HNDLR) & f_onw_fliter)
 async def check_alive(_, message):
     await message.reply_text(ALIVE)
 
 
-@Client.on_message(filters.command("help", COMMAND_HAND_LER) & f_onw_fliter)
+@Client.on_message(filters.command("help", HNDLR) & f_onw_fliter)
 async def help_me(_, message):
     await message.reply_text(HELP)
 
 
-@Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
+@Client.on_message(filters.command("ping", HNDLR) & f_onw_fliter)
 async def ping(_, message):
     start_t = time.time()
     rm = await message.reply_text("...")
@@ -33,6 +33,6 @@ async def ping(_, message):
     await rm.edit(f"Pong!\n{time_taken_s:.3f} ms")
 
 
-@Client.on_message(filters.command("repo", COMMAND_HAND_LER) & f_onw_fliter)
+@Client.on_message(filters.command("repo", HNDLR) & f_onw_fliter)
 async def repo(_, message):
     await message.reply_text(REPO)
