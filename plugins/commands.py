30cfac1bbced70c7c91899d746db726ecaa2e4f7
LordSA <96365910+LordSA@users.noreply.github.com>
2022-03-03 22:10:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -4,6 +4,7 @@ import random
 import asyncio
 import aiohttp
 from Script import script
+from time import time, sleep
 from pyrogram import Client, filters
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
@@ -18,6 +19,8 @@ from database.gtrans_mdb import find_one
 from plugins.Tools.help_func.admin_check import admin_check
 from plugins.Tools.help_func.cust_p_filters import f_onw_fliter
 from plugins.Tools.help_func.cust_p_filters import admin_fliter
+from pyrogram.errors.exceptions.forbidden_403 import ChatWriteForbidden
+from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired, UserAdminInvalid
 import re
 import json
 import base64
@@ -800,3 +803,99 @@ async def unpin(_, message: Message):
     if not message.reply_to_message:
         return
     await message.reply_to_message.unpin()
+
+@Client.on_message(filters.incoming & ~filters.private & filters.command('inkick'))
+def inkick(client, message):
+  user = client.get_chat_member(message.chat.id, message.from_user.id)
+  if user.status == ("creator"):
+    if len(message.command) > 1:
+      input_str = message.command
+      sent_message = message.reply_text(script.START_KICK)
+      sleep(20)
+      sent_message.delete()
+      message.delete()
+      count = 0
+      for member in client.iter_chat_members(message.chat.id):
+        if member.user.status in input_str and not member.status in ('administrator', 'creator'):
+          try:
+            client.kick_chat_member(message.chat.id, member.user.id, int(time() + 45))
+            count += 1
+            sleep(1)
+          except (ChatAdminRequired, UserAdminInvalid):
+            sent_message.edit(script.ADMIN_REQUIRED)
+            client.leave_chat(message.chat.id)
+            break
+          except FloodWait as e:
+            sleep(e.x)
+      try:
+        sent_message.edit(script.KICKED.format(count))
+      except ChatWriteForbidden:
+        pass
+    else:
+      message.reply_text(script.INPUT_REQUIRED)
+  else:
+    sent_message = message.reply_text(script.CREATOR_REQUIRED)
+    sleep(5)
+    sent_message.delete()
+    message.delete()
+
+@Client.on_message(filters.incoming & ~filters.private & filters.command('dkick'))
+def dkick(client, message):
+  user = client.get_chat_member(message.chat.id, message.from_user.id)
+  if user.status == ("creator"):
+    sent_message = message.reply_text(script.START_KICK)
+    sleep(20)
+    sent_message.delete()
+    message.delete()
+    count = 0
+    for member in client.iter_chat_members(message.chat.id):
+      if member.user.is_deleted and not member.status in ('administrator', 'creator'):
+        try:
+          client.kick_chat_member(message.chat.id, member.user.id, int(time() + 45))
+          count += 1
+          sleep(1)
+        except (ChatAdminRequired, UserAdminInvalid):
+          sent_message.edit(script.ADMIN_REQUIRED)
+          client.leave_chat(message.chat.id)
+          break
+        except FloodWait as e:
+          sleep(e.x)
+    try:
+      sent_message.edit(script.DKICK.format(count))
+    except ChatWriteForbidden:
+      pass
+  else:
+    sent_message = message.reply_text(script.CREATOR_REQUIRED)
+    sleep(5)
+    sent_message.delete()
+    message.delete()
+
+@Client.on_message(filters.incoming & ~filters.private & filters.command('instatus'))
+def instatus(client, message):
+  user = client.get_chat_member(message.chat.id, message.from_user.id)
+  if user.status in ('administrator', 'creator', 'ADMINS'):
+    sent_message = message.reply_text(script.FETCHING_INFO)
+    recently = 0
+    within_week = 0
+    within_month = 0
+    long_time_ago = 0
+    deleted_acc = 0
+    uncached = 0
+    bot = 0
+    for member in client.iter_chat_members(message.chat.id):
+      user = member.user
+      if user.is_deleted:
+        deleted_acc += 1
+      elif user.is_bot:
+        bot += 1
+      elif user.status == "recently":
+        recently += 1
+      elif user.status == "within_week":
+        within_week += 1
+      elif user.status == "within_month":
+        within_month += 1
+      elif user.status == "long_time_ago":
+        long_time_ago += 1
+      else:
+        uncached += 1
+    sent_message.edit(script.STATUS.format(message.chat.title, recently, within_week, within_month, long_time_ago, deleted_acc, bot, uncached)
