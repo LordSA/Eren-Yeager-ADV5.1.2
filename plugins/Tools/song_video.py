3ef923d3fae7dc17e6963c77dd9db87c611ee9ea
LordSA <96365910+LordSA@users.noreply.github.com>
2022-03-03 21:42:44 +0530

    Update song_video.py

--- plugins/Tools/song_video.py
+++ plugins/Tools/song_video.py
@@ -57,7 +57,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"[@Lucifer_DevilZ]"
+            performer = f"<a href=https://t.me/lord1of5darkness9>SA GRAPHICS</a>"
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
