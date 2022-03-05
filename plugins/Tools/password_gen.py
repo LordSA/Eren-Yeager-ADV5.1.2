e45ecd8802931de081816041430c872c08f27338
LordSA <96365910+LordSA@users.noreply.github.com>
2022-03-05 13:49:12 +0530

    Update password_gen.py

--- plugins/Tools/password_gen.py
+++ plugins/Tools/password_gen.py
@@ -31,6 +31,6 @@ async def password(bot, update):
         text = "Sorry... Failed To Create Password, Because Limit is 1 to 100."
     else:
         random_value = "".join(random.sample(password, limit))
-        text = f"**Limit :-** `{str(limit)}`.\n**Password :-** `{random_value}`**\n\nJoin @JOSPSupport",
+        text = f"**Limit :-** `{str(limit)}`.\n**Password :-** `{random_value}`**\n\nJoin @mwpro11",
         reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('Movie World', url='https://t.me/mwmoviespro')]])
     await message.edit_text(text, True)
