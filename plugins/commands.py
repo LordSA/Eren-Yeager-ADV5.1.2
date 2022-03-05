b48b26b3254c17bcc379a8abb9b5917d592879e9
LordSA <96365910+LordSA@users.noreply.github.com>
2022-03-05 13:54:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -6,6 +6,8 @@ import aiohttp
 from Script import script
 from time import time, sleep
 from pyrogram import Client, filters
+from urllib.parse import quote
+from info import SUPPORT_CHAT
 from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
@@ -977,3 +979,34 @@ async def password(bot, update):
         text = f"**Limit :-** `{str(limit)}`.\n**Password :-** `{random_value}`**\n\nJoin @mwpro11",
         reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('Movie World', url='https://t.me/mwmoviespro')]])
     await message.edit_text(text, True)	
+
+#Share Text
+
+def share_link(text: str) -> str:
+    return "**Here is Your Sharing Text 👇**\n\nhttps://t.me/share/url?url=" + quote(text)
+
+@Client.on_message(filters.command(["share", "sharetext", "st", "stxt", "shtxt", "shtext"]))
+async def share_text(client, message):
+    reply = message.reply_to_message
+    reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
+    input_split = message.text.split(None, 1)
+    if len(input_split) == 2:
+        input_text = input_split[1]
+    elif reply and (reply.text or reply.caption):
+        input_text = reply.text or reply.caption
+    else:
+        await message.reply_text(
+            text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            "Support Chat", url=f"https://t.me/{SUPPORT_CHAT}")
+                    ]                
+                ]
+            ),
+            reply_to_message_id=reply_id
+        )
+        return
+    await message.reply_text(share_link(input_text), reply_to_message_id=reply_id)
+
