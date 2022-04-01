197521553edf14f4e02cf28138fc2b947c524f50
LordSA <96365910+LordSA@users.noreply.github.com>
2022-04-01 19:35:07 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,6 +8,10 @@ from asyncio import get_running_loop
 from io import BytesIO
 from Script import script
 from time import time, sleep
+from datetime import datetime
+from pyrogram.errors import UserNotParticipant
+from plugins.Tools.help_func.extract_user import extract_user
+from plugins.Tools.help_func.last_online_hlpr import last_online
 from pyrogram import Client, filters
 from urllib.parse import quote
 from info import SUPPORT_CHAT
@@ -17,7 +21,7 @@ from pyrogram.errors import ChatAdminRequired, FloodWait
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from database.ia_filterdb import Media, get_file_details, unpack_new_file_id
 from database.users_chats_db import db
-from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT
+from info import CHANNELS, ADMINS, AUTH_CHANNEL, LOG_CHANNEL, PICS, BATCH_FILE_CAPTION, CUSTOM_FILE_CAPTION, PROTECT_CONTENT,HNDLR
 from utils import get_settings, get_size, is_subscribed, save_group_settings, temp
 from database.connections_mdb import active_connection
 from googletrans import Translator
@@ -1096,3 +1100,71 @@ async def telegraph(client, message):
     )
     finally:
         os.remove(download_location)
+
+@Client.on_message(filters.command("whois", "info") & f_onw_fliter)
+async def who_is(client, message):
+    """ extract user information """
+    status_message = await message.reply_text(
+        "നിക്കു നെൻപ നോക്കട്ടെ 🙂"
+    )
+    from_user = None
+    from_user_id, _ = extract_user(message)
+    try:
+        from_user = await client.get_users(from_user_id)
+    except Exception as error:
+        await status_message.edit(str(error))
+        return
+    if from_user is None:
+        await status_message.edit("no valid user_id / message specified")
+        return
+    
+    first_name = from_user.first_name or ""
+    last_name = from_user.last_name or ""
+    username = from_user.username or ""
+    
+    message_out_str = (
+        "<b>Name:</b> "
+        f"<a href='tg://user?id={from_user.id}'>{first_name}</a>\n"
+        f"<b>Suffix:</b> {last_name}\n"
+        f"<b>Username:</b> @{username}\n"
+        f"<b>User ID:</b> <code>{from_user.id}</code>\n"
+        f"<b>User Link:</b> {from_user.mention}\n" if from_user.username else ""
+        f"<b>Is Deleted:</b> True\n" if from_user.is_deleted else ""
+        f"<b>Is Verified:</b> True" if from_user.is_verified else ""
+        f"<b>Is Scam:</b> True" if from_user.is_scam else ""
+        # f"<b>Is Fake:</b> True" if from_user.is_fake else ""
+        f"<b>Last Seen:</b> <code>{last_online(from_user)}</code>\n\n"
+    )
+
+    if message.chat.type in ["supergroup", "channel"]:
+        try:
+            chat_member_p = await message.chat.get_member(from_user.id)
+            joined_date = datetime.fromtimestamp(
+                chat_member_p.joined_date or time.time()
+            ).strftime("%Y.%m.%d %H:%M:%S")
+            message_out_str += (
+                "<b>Joined on:</b> <code>"
+                f"{joined_date}"
+                "</code>\n"
+            )
+        except UserNotParticipant:
+            pass
+    chat_photo = from_user.photo
+    if chat_photo:
+        local_user_photo = await client.download_media(
+            message=chat_photo.big_file_id
+        )
+        await message.reply_photo(
+            photo=local_user_photo,
+            quote=True,
+            caption=message_out_str,
+            disable_notification=True
+        )
+        os.remove(local_user_photo)
+    else:
+        await message.reply_text(
+            text=message_out_str,
+            quote=True,
+            disable_notification=True
+        )
+    await status_message.delete()
