94a36e77e771cab48cff0cd01fc0ab6773aba64d
LordSA <96365910+LordSA@users.noreply.github.com>
2022-04-03 22:46:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -77,7 +77,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_video(
-            video=random.choice(PICS, VIDS),
+            video=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
