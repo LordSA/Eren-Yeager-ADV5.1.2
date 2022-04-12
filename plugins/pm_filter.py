47fe64497a7bf9d1534a772e153bfa20ada0bb8b
LordSA <96365910+LordSA@users.noreply.github.com>
2022-04-12 14:30:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -917,7 +917,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {file.file_name}", callback_data=f'{pre}#{file.file_id}'
+                    text=f"© {file.file_name} [{get_size(file.file_size)}]", callback_data=f'{pre}#{file.file_id}'
                 ),
             ]
             for file in files
@@ -926,7 +926,7 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"{file.file_name}",
+                    text=f"© {file.file_name}",
                     callback_data=f'{pre}#{file.file_id}',
                 ),
                 InlineKeyboardButton(
