cfe29a7e451ff0d745169cb5fc550cf3617b016d
LordSA <96365910+LordSA@users.noreply.github.com>
2022-05-14 21:29:48 +0530

    Update reporting_admins.py

--- plugins/Tools/reporting_admins.py
+++ plugins/Tools/reporting_admins.py
@@ -3,7 +3,7 @@ import asyncio
 import os
 from pyrogram import filters, Client
 
-@Client.on_message((filters.command('report') | filters.regex("@admins") | filters.regex("@admin")) & filters.group)
+@Client.on_message((filters.command("report") | filters.regex("@admins") | filters.regex("@admin")) & filters.group)
 async def report_user(bot, message):
     if message.reply_to_message:
         chat_id = message.chat.id
