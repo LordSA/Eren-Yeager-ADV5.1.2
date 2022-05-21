9a9bc5952b4bc0989fe9a79f644e1a1983473891
LordSA <96365910+LordSA@users.noreply.github.com>
2022-05-21 23:20:56 +0530

    Create channeladmins.py

---
+++ VC/modules/channeladmins.py
@@ -0,0 +1,200 @@
+from asyncio import QueueEmpty
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pytgcalls.types.input_stream import AudioPiped
+
+from VC.function.admins import set
+from VC.helpers.decorators import authorized_users_only, errors
+from VC.services.pytgcalls import pytgcalls
+from VC.services.queues import queues
+
+ACTV_CALLS = []
+
+
+@Client.on_message(
+    filters.command(["channelpause", "cpause"]) & filters.group & ~filters.edited
+)
+@errors
+@authorized_users_only
+async def pause(_, message: Message):
+    try:
+        conchat = await _.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
+    except:
+        await message.reply("Is chat even linked")
+        return
+    chat_id = chid
+    for x in pytgcalls.active_calls:
+        ACTV_CALLS.append(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALLS:
+        await message.reply_text("❗ Nothing is playing!")
+    else:
+        await pytgcalls.pause_stream(chat_id)
+        await message.reply_text("▶️ Paused!")
+
+
+@Client.on_message(
+    filters.command(["channelresume", "cresume"]) & filters.group & ~filters.edited
+)
+@errors
+@authorized_users_only
+async def resume(_, message: Message):
+    try:
+        conchat = await _.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
+    except:
+        await message.reply("Is chat even linked")
+        return
+    chat_id = chid
+    for x in pytgcalls.active_calls:
+        ACTV_CALLS.append(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALLS:
+        await message.reply_text("❗ Nothing is paused!")
+    else:
+        await pytgcalls.resume_stream(chat_id)
+        await message.reply_text("⏸ Resumed!")
+
+
+@Client.on_message(
+    filters.command(["channelend", "cend"]) & filters.group & ~filters.edited
+)
+@errors
+@authorized_users_only
+async def stop(_, message: Message):
+    try:
+        conchat = await _.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
+    except:
+        await message.reply("Is chat even linked")
+        return
+    chat_id = chid
+    for x in pytgcalls.active_calls:
+        ACTV_CALLS.append(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALLS:
+        await message.reply_text("❗ Nothing is streaming!")
+    else:
+        try:
+            queues.clear(message.chat.id)
+        except QueueEmpty:
+            pass
+
+        await pytgcalls.leave_group_call(chat_id)
+        await message.reply_text("❌ Stopped streaming!")
+
+
+@Client.on_message(
+    filters.command(["channelskip", "cskip"]) & filters.group & ~filters.edited
+)
+@errors
+@authorized_users_only
+async def skip(_, message: Message):
+    global que
+    try:
+        conchat = await _.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
+    except:
+        await message.reply("Is chat even linked")
+        return
+    chat_id = chid
+    for x in pytgcalls.active_calls:
+        ACTV_CALLS.append(int(x.chat_id))
+    if int(chat_id) not in ACTV_CALLS:
+        await message.reply_text("❗ Nothing is playing to skip!")
+    else:
+        queues.task_done(chat_id)
+
+        if queues.is_empty(chat_id):
+            await pytgcalls.leave_group_call(chat_id)
+        else:
+            await pytgcalls.change_stream(
+                chat_id,
+                AudioPiped(
+                    queues.get(chat_id)["file"],
+                ),
+            )
+
+    qeue = que.get(chat_id)
+    if qeue:
+        skip = qeue.pop(0)
+    if not qeue:
+        return
+    await message.reply_text(f"- Skipped **{skip[0]}**\n- Now Playing **{qeue[0][0]}**")
+
+
+@Client.on_message(
+    filters.command(["channelmute", "cmute"]) & filters.group & ~filters.edited
+)
+@errors
+@authorized_users_only
+async def mute(_, message: Message):
+    global que
+    try:
+        conchat = await _.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
+    except:
+        await message.reply("Is chat even linked")
+        return
+    chat_id = chid
+    result = await pytgcalls.mute_stream(chat_id)
+    await message.reply_text("✅ Muted")
+    if mute:
+        result == 0
+    else:
+        await message.reply_text("❌ Already muted")
+    if not mute:
+        result == 1
+    else:
+        await message.reply_text("❌ Not in call")
+
+
+@Client.on_message(
+    filters.command(["channelunmute", "cunmute"]) & filters.group & ~filters.edited
+)
+@errors
+@authorized_users_only
+async def unmute(_, message: Message):
+    global que
+    try:
+        conchat = await _.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
+    except:
+        await message.reply("Is chat even linked")
+        return
+    chat_id = chid
+    result = await pytgcalls.unmute_stream(chat_id)
+    await message.reply_text("✅ Unmuted")
+    if unmute:
+        result == 0
+    else:
+        await message.reply_text("❌ Not muted")
+    if not unmute:
+        result == 1
+    else:
+        await message.reply_text("❌ Not in call")
+
+
+@Client.on_message(filters.command("channeladmincache"))
+@errors
+async def admincache(client, message: Message):
+    try:
+        conchat = await client.get_chat(message.chat.id)
+        conid = conchat.linked_chat.id
+        chid = conid
+    except:
+        await message.reply("Is chat even linked")
+        return
+    set(
+        chid,
+        [
+            member.user
+            for member in await conchat.linked_chat.get_members(filter="administrators")
+        ],
+    )
+    await message.reply_text("❇️ Admin cache refreshed!")
