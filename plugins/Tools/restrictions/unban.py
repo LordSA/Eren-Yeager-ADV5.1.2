358cfb17752ce32fbfd72344df992d43e197cdae
LordSA <96365910+LordSA@users.noreply.github.com>
2022-07-21 15:26:15 +0530

    Update unban.py

--- plugins/Tools/restrictions/unban.py
+++ plugins/Tools/restrictions/unban.py
@@ -3,7 +3,7 @@ from plugins.Tools.help_func.admin_check import admin_check
 from plugins.Tools.help_func.extract_user import extract_user
 
 
-@Client.on_message(filters.command(["unban", "unmute"]))
+@Client.on_message(filters.command(["gunban", "gunmute"]))
 async def un_ban_user(_, message):
     is_admin = await admin_check(message)
     if not is_admin:
