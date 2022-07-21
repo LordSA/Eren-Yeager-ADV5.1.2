da0723c98cec40b52470170ed5a09ff2977915c7
LordSA <96365910+LordSA@users.noreply.github.com>
2022-07-21 12:54:23 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -106,7 +106,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
-            photo=random.choice(PICS),
+            photo=random.choice(VIDS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
