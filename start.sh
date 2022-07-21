c495ede02d23392fd6ba33af5cc3e3faaa78e1a1
LordSA <96365910+LordSA@users.noreply.github.com>
2022-07-21 11:48:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LordSA/movie-world.git /movie-world
+  git clone https://github.com/LordSA/Eren-Yeager-ADV5.1.2.git /Eren-Yeager-ADV5.1.2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /movie-world
+  git clone $UPSTREAM_REPO /Eren-Yeager-ADV5.1.2
 fi
 cd /movie-world
 pip3 install -U -r requirements.txt
