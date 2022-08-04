3594e4acad3fea6b95f251d2e990c555334ddb31
LordSA <96365910+LordSA@users.noreply.github.com>
2022-08-04 20:55:02 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -17,6 +17,7 @@ async def disabled_chat(_, client, message: Message):
 
 disabled_group=filters.create(disabled_chat)
 
+
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
     ban = await db.get_ban_status(message.from_user.id)
