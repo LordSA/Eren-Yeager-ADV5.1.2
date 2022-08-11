fbb6fbc16fbf905f8e64ad0f3b1b01bc806b7ab9
LordSA <96365910+LordSA@users.noreply.github.com>
2022-08-11 09:48:45 +0530

    V2

--- plugins/commands.py
+++ plugins/commands.py
@@ -79,11 +79,11 @@ async def start(client, message):
 
         if message.command[1] != "subscribe":
             try:
-            	kk, file_id = message.command[1].split("_", 1)
-                pre = 'checksubp' if kk == 'filep' else 'checksub'
-                btn.append([InlineKeyboardButton(" 🔄 『𝚃𝚁𝚈 𝙰𝙶𝙰𝙸𝙽』", callback_data=f"{pre}#{file_id}")])
+                kk, file_id = message.command[1].split("_", 1)
+                pre = 'checksubp' if kk == 'filep' else 'checksub' 
+                btn.append([InlineKeyboardButton("🔄 『𝚃𝚁𝚈 𝙰𝙶𝙰𝙸𝙽』", callback_data=f"{pre}#{file_id}")])
             except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton(" 🔄 『𝚃𝚁𝚈 𝙰𝙶𝙰𝙸𝙽』", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
+                btn.append([InlineKeyboardButton("🔄 『𝚃𝚁𝚈 𝙰𝙶𝙰𝙸𝙽』", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_message(
             chat_id=message.from_user.id,
             text="**Please Join My Updates Channel to use this Bot!**",
